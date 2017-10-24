28840c2fc0fc57e4293b6970d5324fb6b6ec073a
David Kuruvilla <dapawn@gmail.com>
2017-10-24 20:27:02 +0000

    automatically backed up by learn

--- lib/book.rb
+++ lib/book.rb
@@ -2,21 +2,19 @@
 class Book
   attr_accessor :author, :page_count  # remove the attr_accessor for genre
   attr_reader :title, :genre  # add an attr_reader for genre
- 
+
   GENRES = []
- 
+
   def initialize(title)
     @title = title
   end
- 
+
   def turn_page
     puts "Flipping the page...wow, you read fast!"
   end
- 
+
   def genre=(genre)
     @genre = genre
-    GENRES << genre 
+    GENRES << genre
   end
 end
-
-
